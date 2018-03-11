d162a62c87be6e2ef6bacb2149b9751f861a26cd
Roger Ballard <roger@park-it-solutions.com>
2018-03-11 12:00:51 +0000

    Token model progress

--- src/models/token.js
+++ src/models/token.js
@@ -1,38 +1,43 @@
+
+import store from '../utils/store'
 /**
  * Define token model
  * @type {Object}
  */
 const token = {
   state: {
+    compiled: null,
     contract: null,
-    abi: null
+    instance: null
   },
   reducers: {
-    setAbi (state, payload) {
+    setCompiled (state, payload) {
       return {
         ...state,
-        abi: payload
+        compiled: payload
+      }
+    },
+    setContract (state, payload) {
+      return {
+        ...state,
+        contract: payload
+      }
+    },
+    setInstance (state, payload) {
+      return {
+        ...state,
+        instance: payload
       }
     }
   },
   effects: {
-    async compile (payload, rootState) {
-      let web3 = rootState.web3.instance
-      let contract = rootState.token.contract
-      console.log(contract)
-      console.log(web3.eth.compile.solidity(contract))
-        // .then(abi => {
-        //   console.log(abi)
-        //   this.setAbi(abi)
-        // })
-      // console.log(abi)
-      // this.setAbi(abi)
-    },
     async deploy (payload, rootState) {
-      let web3 = rootState.web3.instance
-      let abi = rootState.token.abi
-      let TokenContract = await web3.eth.contract(abi)
+      const compiled = require('../utils/Token.json')
+      this.setCompiled(compiled)
       debugger
+      let web3 = store.getState().web3.instance
+      let contract = new web3.eth.Contract(compiled.abi)
+      this.setContract(contract)
     }
   }
 }
